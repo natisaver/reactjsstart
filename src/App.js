d8bf197643336f5d3bfe67ca4297426048903db9
nathaniel <64686163+natisaver@users.noreply.github.com>
2021-07-07 04:56:43 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+    <Header />
+    <main>
+      <div className="App">
+      </div>
+    </main>
+    <Footer />
+    </>
+  );
+}
+
+export default App;
